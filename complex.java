2ddf165df9548beddead5122fdc051039880108d
ROSHNA-JOSHY36 <76092006+ROSHNA-JOSHY36@users.noreply.github.com>
2021-05-18 03:32:54 -0700

    Add files via upload

---
+++ complex.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class complex{
+	double r,i;
+public static void main(String a[])
+{
+	Scanner sc=new Scanner(System.in);
+	complex c=new complex();
+	complex c1=new complex();
+	System.out.println("Complex number a:");
+	c.r=sc.nextDouble();
+	c.i=sc.nextDouble();
+	System.out.println("Complex number b:");
+	c1.r=sc.nextDouble();
+	c1.i=sc.nextDouble();
+	double smr=c1.r+c.r;
+	double smi=c1.i+c.i;
+	System.out.println("Sum:"+smr+"+"+smi+"i");
+}
+}
---
+++ employee.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class employee {
+	int eno,esalary;
+	String ename;
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		employee[] em=new employee[3];
+		System.out.println("Limit");
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			System.out.println("Enter the details:");
+			em[i]=new employee();
+			em[i].eno=sc.nextInt();
+			em[i].esalary=sc.nextInt();
+			em[i].ename=sc.next();
+		}
+		System.out.println("Enter the eno:");
+		int enoo=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			if(em[i].eno==enoo)
+			{
+				System.out.println("Employee found");
+				break;
+			}
+			if(i==n-1)
+			{
+				System.out.println("Employee not found:");
+			}
+		}
+
+
+	}
+
+}
